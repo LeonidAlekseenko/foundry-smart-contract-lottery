6b3e830a125a02a7d6495fa7f03aed31f62ce83f
LeonidAlekseenko <bvqsdd@mail.ru>
2026-01-15 22:17:14 +0300

    initial vrf

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,32 +1,51 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-
-import {VRFConsumerBaseV2Plus} from "@chainlink/contracts@1.1.1/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 /**
-* @title A smample Raffle contract
-* @author Alekseenko Leonid 
-* @notice This contract is for creating a sample rafle
-* @dev ImplementsChainLincVFRv2.5
-*/
+ * @title A smample Raffle contract
+ * @author Alekseenko Leonid
+ * @notice This contract is for creating a sample rafle
+ * @dev ImplementsChainLincVFRv2.5
+ */
 
-contract Raffle {  
-
+contract Raffle is VRFConsumerBaseV2Plus {
     error Raffle_SendMoreToEnterRaffle();
+
     uint256 private immutable i_entranceFee;
     // @dev The duration of the lottery in second
     uint256 private immutable i_interval;
     uint256 private s_lastTimeStamp;
     address payable[] private s_palayers;
-    event RaffleEntered(address indexed player);
 
-    constructor(uint256 entranceFee, uint256 interval) {
+    event RaffleEntered(address indexed player);
+    //function data
+
+    bytes32 private immutable i_keyHash;
+    uint256 private immutable i_subscriptionId;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private immutable i_callbackGasLimit;
+    uint32 private constant NUM_WORDS = 1;
+
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint256 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2Plus(vrfCoordinator) {
         i_entranceFee = entranceFee;
         i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
+        i_keyHash = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffle() external payable {
-        if(msg.value < i_entranceFee) {
+        if (msg.value < i_entranceFee) {
             revert Raffle_SendMoreToEnterRaffle();
         }
         s_palayers.push(payable(msg.sender));
@@ -34,24 +53,28 @@ contract Raffle {
     }
 
     function pickWinner() external {
-        if((block.timestamp - s_lastTimeStamp) < i_interval) {
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
             revert();
         }
-        requestId = s_vrfCoordinator.requestRandomWords(
-        VRFV2PlusClient.RandomWordsRequest({
-            keyHash: keyHash,
-            subId: s_subscriptionId,
-            requestConfirmations: requestConfirmations,
-            callbackGasLimit: callbackGasLimit,
-            numWords: numWords,
-            extraArgs: VRFV2PlusClient._argsToBytes(VRFV2PlusClient.ExtraArgsV1({nativePayment: enableNativePayment}))
-            })
-        );
+
+        VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient.RandomWordsRequest({
+            keyHash: i_keyHash,
+            subId: i_subscriptionId,
+            requestConfirmations: REQUEST_CONFIRMATIONS,
+            callbackGasLimit: i_callbackGasLimit,
+            numWords: NUM_WORDS,
+            extraArgs: VRFV2PlusClient._argsToBytes(
+                VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                )
+            });
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
     }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] calldata randomWords) internal override {}
 
     /* Getters **/
 
-    function getEntranceFee() external view returns (uint256){
+    function getEntranceFee() external view returns (uint256) {
         return i_entranceFee;
     }
 }
--- test/staging/RaffleStagingTest.t.sol
+++ test/staging/RaffleStagingTest.t.sol
@@ -1,11 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
+
 import {Test, console} from "forge-std/Test.sol";
 
-
-contract RaffleTest is Test {
-
-
-
-
-}
+contract RaffleTest is Test {}
